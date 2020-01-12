b24658c0e4ab104c1296102b3d0386a3a0376a35
sangram <sangramvbarge@gmail.com>
2020-01-12 19:56:34 +0530

    Updates

---
+++ precache-manifest.6b8f482bb10c7a9aa447fd35f995b1f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1947628898ad97ea032887508b618cbd",
+    "url": "/sangrambarge/home/index.html"
+  },
+  {
+    "revision": "ea85646056f7bd09352c",
+    "url": "/sangrambarge/home/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f4abdda7cf76b3c1ed6a",
+    "url": "/sangrambarge/home/static/js/2.3f62d6d1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sangrambarge/home/static/js/2.3f62d6d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea85646056f7bd09352c",
+    "url": "/sangrambarge/home/static/js/main.7edbdca5.chunk.js"
+  },
+  {
+    "revision": "5b1efc17f2d6b5717f5b",
+    "url": "/sangrambarge/home/static/js/runtime-main.bf4a45c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sangrambarge/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.8b2f45679ea597829002999304fa227d.js"
+  "/sangrambarge/home/precache-manifest.6b8f482bb10c7a9aa447fd35f995b1f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sangrambarge/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
